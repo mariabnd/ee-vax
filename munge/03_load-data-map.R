331898b40de5ea4201a7cb2ae10af835656c1e2e
Maria <33788128+mariabnd@users.noreply.github.com>
2023-04-04 18:21:45 +0200

    Fix map plotting issue

--- munge/03_load-data-map.R
+++ munge/03_load-data-map.R
@@ -1,17 +1,12 @@
 # MAP
 # Source: https://github.com/ebp-group/Switzerland_Tilemap
 # Tilemap CC-BY www.ebp.ch
-shp <- read_sf("data/Switzerland_Tiles_CH1903LV03.shp")
-# Remove the lakes
-shp <- shp[!c(shp$Name %in% c("Bodensee", "Lac Léman")), ]
-names(shp)[names(shp) == "Name"] <- "geoRegion"
-
 # As SpatialPolygonsDataFrame
 mapCH <- readShapePoly("data/Switzerland_Tiles_CH1903LV03.shp")
-# Remove same parts as before
+# Remove the lakes
 mapCH <- mapCH[!c(mapCH$Name %in% c("Bodensee", "Lac Léman")), ]
-names(mapCH)[names(mapCH) == "Name"] <- "NAME"
-mapCH$NAME <- as.character(mapCH$NAME)
+names(mapCH)[names(mapCH) == "Name"] <- "geoRegion"
+mapCH$NAME <- as.character(mapCH$geoRegion)
 row.names(mapCH) <- unique(mapCH$NAME)
 
 rm(shp)
